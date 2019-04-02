b3f7626ecaee1afd59799cb5325601fb00ba3f59
David W. Green <david@davidwgreen.com>
2019-04-02 14:40:23 -0400

    Initial commit.

--- patron-counts-new-ptype.sql
+++ patron-counts-new-ptype.sql
@@ -1,7 +1,14 @@
 SET TIMEZONE = 'America/New_York';
-SELECT count(DISTINCT pr.id)
-FROM sierra_view.patron_record pr
-JOIN sierra_view.record_metadata rm on pr.id = rm.id
--- change to desired PTYPE
-WHERE pr.ptype_code = e'3' AND
-date_trunc('month', rm.creation_date_gmt) = date_trunc('month', now()) - interval '1 month';
+
+SELECT (SELECT count(DISTINCT pr.id)
+        FROM sierra_view.patron_record pr
+               JOIN sierra_view.record_metadata rm on pr.id = rm.id
+        WHERE pr.ptype_code = e'0'
+          AND date_trunc('month', rm.creation_date_gmt) = date_trunc('month', now()) - interval '1 month'
+       ) as "Total State",
+
+       (SELECT count(DISTINCT pr.id)
+        FROM sierra_view.patron_record pr
+               JOIN sierra_view.record_metadata rm on pr.id = rm.id
+        WHERE pr.ptype_code = e'3'
+          AND date_trunc('month', rm.creation_date_gmt) = date_trunc('month', now()) - interval '1 month' ) as "Total Resident";
