383fcbfe33a25b66a73a8579331fabb518b46f71
David W. Green <david@davidwgreen.com>
2019-05-20 15:02:58 -0400

    Initial commit

---
+++ patron-new-labels-2.sql
@@ -0,0 +1,32 @@
+WITH valid_ids AS (
+    SELECT DISTINCT record_id
+    FROM sierra_view.varfield
+    WHERE record_id > 481037417718
+      AND varfield_type_code = 's'
+      AND field_content IS NOT NULL
+      AND field_content != ''
+)
+SELECT DISTINCT pv.record_num,
+                fn.first_name,
+                fn.last_name,
+                pra.addr1,
+                pra.addr2,
+                pra.city,
+                pra.region,
+                pra.postal_code
+FROM sierra_view.patron_view pv
+         JOIN sierra_view.patron_record_address pra
+              ON pra.patron_record_id = pv.id
+         JOIN sierra_view.patron_record_address_type prat
+              ON prat.id = pra.patron_record_address_type_id
+         JOIN sierra_view.patron_record_fullname fn
+              ON fn.patron_record_id = pv.id
+         JOIN sierra_view.varfield vf ON vf.record_id = pv.id
+WHERE record_id > 481037417718
+  AND pv.ptype_code <= 3
+  AND prat.code = 'a'
+  AND pv.id NOT IN (
+    SELECT record_id
+    FROM valid_ids
+)
+ORDER BY 3 ASC;
