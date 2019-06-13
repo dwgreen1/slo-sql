32f9f6a3c9f0fb990012a3faaa58c20b6df1dab8
David W. Green <david@davidwgreen.com>
2019-06-13 13:06:33 -0400

    More accurate subfield.content match

--- authority-subfield-0-entries.sql
+++ authority-subfield-0-entries.sql
@@ -1,6 +1,6 @@
-SELECT DISTINCT REGEXP_REPLACE(subfield.content, '^https?:\/\/id.loc.gov\/authorities\/.*\/', '', 'gi')
+SELECT DISTINCT REGEXP_REPLACE(subfield.content, '^https?:\/\/id.loc.gov\/authorities\/.*\/', '', 'gi') AS "ARN"
 FROM sierra_view.subfield
 WHERE subfield.tag = '0'
-AND subfield.content LIKE 'http%'
+AND subfield.content LIKE 'http%id.loc.gov%'
 AND subfield.marc_tag = '100'
 LIMIT 50;
