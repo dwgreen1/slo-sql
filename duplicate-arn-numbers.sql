82c590ab44e89bf79597059cd9387ee4dca36887
David W. Green <david@davidwgreen.com>
2019-06-25 11:56:05 -0400

    Added additional, useful statement

--- duplicate-arn-numbers.sql
+++ duplicate-arn-numbers.sql
@@ -1,15 +1,50 @@
 -- Duplicate ARN Numbers
-SELECT pe.index_entry        AS "ARN #",
-       COUNT(pe.index_entry) AS "COUNT"
+SELECT id2reckey(pe.record_id), pe.index_entry
 FROM sierra_view.phrase_entry pe
-  WHERE varfield_type_code = 'o'
-  AND index_tag = 'z'
-  AND parent_record_id IN (
-      SELECT v.record_id
+WHERE pe.index_entry IN (
+    SELECT pe.index_entry AS "ARN #"
+    FROM sierra_view.phrase_entry pe
+    WHERE varfield_type_code = 'o'
+      AND index_tag = 'z'
+      AND parent_record_id IN (
+        SELECT v.record_id
         FROM sierra_view.varfield v
-        -- Switch between names / subjects
-       WHERE v.varfield_type_code = 'q'
-  )
-GROUP BY index_entry
-HAVING (COUNT(index_entry) > 1)
-ORDER BY 2 DESC, 1 ASC;
+             -- Switch between names / subjects
+        WHERE v.varfield_type_code = 'd'
+    )
+    GROUP BY index_entry
+    HAVING (COUNT(index_entry) > 1)
+)
+ORDER BY 2;
+
+-- Duplicate ARN Numbers Pt. II
+WITH dup_name
+         AS (SELECT a.code1,
+                    e.index_tag,
+                    e.index_entry
+             FROM sierra_view.phrase_entry AS e
+                      JOIN sierra_view.record_metadata AS r
+                           ON r.id = e.record_id
+                               AND r.record_type_code = 'a'
+                      JOIN sierra_view.authority_record a
+                           ON a.record_id = e.record_id
+             WHERE e.index_tag = 'z'
+               AND e.varfield_type_code = 'o'
+              -- AND a.code1 = 'q'
+             GROUP BY a.code1,
+                      e.index_tag,
+                      e.index_entry
+             HAVING COUNT(*) > 1)
+SELECT d.*,
+       r.record_type_code
+           || r.record_num
+           || 'a' AS record_num
+FROM dup_name AS d
+         JOIN sierra_view.phrase_entry AS e
+              ON e.index_tag
+                     || e.index_entry = d.index_tag
+                     || d.index_entry
+         JOIN sierra_view.record_metadata AS r
+              ON r.id = e.record_id
+                  AND r.record_type_code = 'a'
+ORDER BY 1, 3, 4;
