08d530d4270da64490e9645b3696aec9f46f8c5b
David W. Green <david@davidwgreen.com>
2019-10-10 08:09:02 -0400

    Add duplicate details back

--- patron-duplicate-emails.sql
+++ patron-duplicate-emails.sql
@@ -1,7 +1,17 @@
-SELECT n.last_name     AS "Last Name",
-       n.first_name    AS "First Name",
-       v.field_content AS "Email Address"
-FROM sierra_view.patron_record_fullname n
-         JOIN
-     sierra_view.varfield v ON v.record_id = n.patron_record_id
-WHERE v.varfield_type_code = 'z';
+SELECT id2reckey(pv.id) || 'a' AS "Record Number",
+       sv.field_content        AS "Email Address",
+       n.last_name             AS "Last Name",
+       n.first_name            AS "First Name"
+FROM sierra_view.patron_view pv
+       JOIN
+     sierra_view.patron_record_fullname n ON n.patron_record_id = pv.id
+       JOIN
+     sierra_view.varfield sv ON sv.record_id = pv.id
+WHERE sv.field_content IN (
+  SELECT sv.field_content
+  FROM sierra_view.varfield sv
+  WHERE sv.varfield_type_code = 'z'
+    AND sv.field_content NOT LIKE '%library%'
+  GROUP BY sv.field_content
+  HAVING (COUNT(sv.field_content) > 1))
+  ORDER BY 2, 3, 4;
