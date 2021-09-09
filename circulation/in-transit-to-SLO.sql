7c3ba9e90fb49b7884f8c9be1c1217f8a0dbb7c8
dgreen <whatever@davidwgreen.com>
2021-09-09 12:23:04 -0400

    Initial commit

---
+++ circulation/in-transit-to-SLO.sql
@@ -0,0 +1,11 @@
+SELECT id2reckey(h.patron_record_id) AS "Patron",
+       id2reckey(h.record_id) AS "Item",
+       ir.location_code AS "Item Location",
+       h.placed_gmt::date AS "Date Placed",
+       h.pickup_location_code
+FROM sierra_view.item_record ir
+         JOIN sierra_view.hold h
+              ON h.record_id = ir.record_id
+WHERE ir.item_status_code = 't'
+  AND pickup_location_code IN ('slo', 'mail', 'sls', 'bwc')
+ORDER BY 5, 4 DESC, 1, 3 DESC;
