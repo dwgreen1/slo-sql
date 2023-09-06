a9d3e5744be344eab371899ec229f82c9e2cb8bb
dwgreen <whatever@davidwgreen.com>
2023-09-06 08:42:18 -0400

    Initial commit

---
+++ cataloging/latest-audiobooks.sql
@@ -0,0 +1,10 @@
+-- OverDrive Audiobooks in past 60 days
+SELECT id2reckey(rm.id)
+FROM sierra_view.record_metadata rm
+JOIN sierra_view.bib_record br ON br.id = rm.id
+JOIN sierra_view.bib_record_location brl ON brl.bib_record_id = br.record_id
+WHERE rm.creation_date_gmt >= CURRENT_DATE - INTERVAL '60 days'
+  AND brl.location_code = 'odl'
+  AND br.bcode2 = 'i'
+ORDER BY rm.creation_date_gmt DESC
+LIMIT 100;
---
+++ cataloging/latest-print.sql
@@ -0,0 +1,9 @@
+-- Print Titles in past 90 days
+SELECT id2reckey(rm.id)
+FROM sierra_view.record_metadata rm
+JOIN sierra_view.bib_record br ON br.id = rm.id
+JOIN sierra_view.bib_record_location brl ON brl.bib_record_id = br.record_id
+WHERE rm.creation_date_gmt >= CURRENT_DATE - INTERVAL '120 days'
+  AND brl.location_code IN ('osd', 'slcl', 'slctr', 'slr', 'sls')
+ORDER BY rm.creation_date_gmt DESC
+LIMIT 100;
